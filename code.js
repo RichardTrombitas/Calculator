f78b9775351f8086c1625606b009dd5d47ae7a16
RichardTrombitas <richard.trombitas@yahoo.com>
2019-08-24 22:20:11 +0300

    initial commit

---
+++ code.js
@@ -0,0 +1,11 @@
+function makeBright(id) {
+    var button = document.getElementById(id);
+    button.style.backgroundColor = "#ec3f3f";
+}
+
+window.onload = init;
+
+function init() {
+    var buttons = document.getElementsByTagName("td");
+
+}
