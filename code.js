49b76e45715919c253193ed3d891ee6ca1f93ac3
RichardTrombitas <richard.trombitas@yahoo.com>
2019-08-27 02:41:14 +0300

    merged visualEffects into code

--- code.js
+++ code.js
@@ -8,6 +8,21 @@ window.onload = function(){
     }
 }
 
+function makeBright(eventObject) {
+    var button = document.getElementById(eventObject.target.id);
+    button.style.backgroundColor = "#ec3f3f";
+}
+
+function makeDim(eventObject) {
+    var button = document.getElementById(eventObject.target.id);
+    button.style.backgroundColor = "#c02e2e";
+}
+
+function makeDimmer(id) {
+    var button = document.getElementById(id);
+    button.style.backgroundColor = "#641818";
+}
+
 var resultElement = document.getElementById("result"); // calculator screen
 
 var operationsArray = ["0"];
